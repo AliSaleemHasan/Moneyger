21180f4dc68357bc11fdc1f4cc8c6f08d66dec79
= <ali1saleem1hasan@outlook.com>
2025-04-14 22:23:45 +0200

    Simple docker compose addition
    - a dockerfile was added for building and running the next js application
    (not yet linked with compose)
    - docker compose file was added for database and dbms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { SignupForm } from "@/components/ui/signup-form";
 
 export default function Home() {
   return (
-    <div>Initial project</div>
+    <div>
+    <div>TY, heheee,this is test</div>
+    </div>
   );
 }
